d94a09b3f404d995d786de11b198cb791ab17842
jdrubin91 <jd.rubin91@gmail.com>
2016-05-19 14:38:04 -0600

    updates

--- src/create_intersects.py
+++ src/create_intersects.py
@@ -4,10 +4,9 @@ import os
 
 def run(histones,bidirectional,TF,mapped):
     histonelist = list()
-    for folder in os.listdir(histones):
-        for file1 in os.listdir(histones + folder):
-            if 'mp' in file1 and 'tdf' not in file1:
-                histonelist.append(histones + folder + '/' + file1)
+    for file1 in os.listdir(histones):
+        if 'mp' in file1 and 'tdf' not in file1:
+            histonelist.append(histones + file1)
 
     for item in histonelist:
         os.system("bedtools map -a " + TF + " -b " + item + " > " + mapped + item.split('/')[-1])
