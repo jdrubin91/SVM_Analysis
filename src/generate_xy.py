610cace1e67305e48db411e450b2cbe190d60db9
jdrubin91 <jd.rubin91@gmail.com>
2016-05-20 11:14:29 -0600

    updates

--- src/generate_xy.py
+++ src/generate_xy.py
@@ -8,19 +8,19 @@ def run(directory):
         feature_names = F.readline().split()
         Y = eval(F.readline())
         X = eval(F.readline())
-    d = dict()
-    for i in range(len(Y)):
-        if Y[i] == 1:
-            d[i] = list()
-    while len(d) < sum(Y)*2:
-        d[int(np.random.uniform(0,len(Y)))] = list()
-    indexes = list()
-    for key in d:
-        indexes.append(key)
-    Y1 = list()
-    X1 = list()
-    for item in indexes:
-        Y1.append(Y[item])
-        X1.append(X[item])
+    #d = dict()
+    #for i in range(len(Y)):
+    #    if Y[i] == 1:
+    #        d[i] = list()
+    #while len(d) < sum(Y)*2:
+    #    d[int(np.random.uniform(0,len(Y)))] = list()
+    #indexes = list()
+    #for key in d:
+    #    indexes.append(key)
+    #Y1 = list()
+    #X1 = list()
+    #for item in indexes:
+    #    Y1.append(Y[item])
+    #    X1.append(X[item])
         
-    return X1,Y1,feature_names
+    return X,Y,feature_names
