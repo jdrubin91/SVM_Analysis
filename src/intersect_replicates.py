8e3ec9b6f890d9a8bf927cd1e69f959e183d6e4f
jdrubin91 <jd.rubin91@gmail.com>
2016-06-21 13:37:06 -0600

    Updates

--- src/intersect_replicates.py
+++ src/intersect_replicates.py
@@ -9,7 +9,7 @@ def run(directory,temp):
         for file1 in os.listdir(directory + folder):
             if 'peaks.bed' in file1:
                 replicates.append(directory + folder + '/' + file1)
-        a = pybt.BedTools(replicates[0])
+        a = pybt.BedTool(replicates[0])
         for i in range(len(replicates)-1):
             a = a.intersect(pybt.BedTool(replicates[i+1]))
         outfile = open(temp + folder + '.bed','w')
