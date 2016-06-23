d98f0961fec2747e10b1ab99d8c65afc65f6b7d4
jdrubin91 <jd.rubin91@gmail.com>
2016-06-23 12:50:01 -0600

    updates

--- src/analyze_SVM.py
+++ src/analyze_SVM.py
@@ -11,6 +11,7 @@ def run(files,figures):
     outfile = open(figures + 'TFs.txt','w')
     for file1 in os.listdir(files):
         TFs = list()
+        TFs2 = list()
         hist = list()
         pos = 0
         neg = 0
@@ -18,25 +19,36 @@ def run(files,figures):
             names = F.readline().strip().split()[1:]
             one = [0] * len(names)
             zero = [0] * len(names)
+            alist = [0] * len(names)
             for line in F:
                 line = line.strip().split()[3:]
+                for i in range(len(line[1:])):
+                    if int(line[i+1]) > 0:
+                        alist[i] += 1.0
                 if line[0] == '0':
                     neg += 1.0
                     for i in range(len(line[1:])):
                         if int(line[i+1]) == 0:
-                            zero[i] += 1
+                            zero[i] += 1.0
                 else:
                     pos += 1.0
                     for i in range(len(line[1:])):
                         if int(line[i+1]) != 0:
-                            one[i] += 1
+                            one[i] += 1.0
         
         for i in range(len(one)):
-            S = ((float(one[i])/pos) + (float(zero[i])/neg))/2
+            N = pos + neg
+            p = pos/N
+            a = alist[i]/N
+            S = one[i] - N*p*a
+            S2 = zero[i] - N*(1-p)*(1-a)
+            #S = ((float(one[i])/pos) + (float(zero[i])/neg))/2
             TFs.append((names[i],S))
+            TFs2.append((names[i],S2))
         hist = [x[1] for x in TFs]
+        hist2 = [x[1] for x in TFs2]
         TFs.sort(key=lambda x: x[1], reverse=True)
-        print file1, len(TFs)
+        TFs2.sort(key=lambda x: x[1], reverse=True)
         outfile.write(file1.split('.')[0] + '\t')
         for item in TFs:
             outfile.write(item[0] + "," + str(item[1]) + ",")
@@ -52,6 +64,18 @@ def run(files,figures):
         the_table = ax2.table(cellText=TFs[0:27], colLabels=colLabels,loc='center',fontsize=1)
         plt.savefig(figures + file1.split('.')[0] + '.png')
         plt.close()
+        
+        F1 = plt.figure()
+        ax1 = F1.add_subplot(121)
+        ax1.hist(hist2,50)
+        ax2 = F1.add_subplot(122)
+        ax2.xaxis.set_visible(False)
+        ax2.yaxis.set_visible(False)
+        colLabels=("TF","S-Score")
+        the_table = ax2.table(cellText=TFs2[0:27], colLabels=colLabels,loc='center',fontsize=1)
+        plt.savefig(figures + file1.split('.')[0] + '_2.png')
+        plt.close()
+        
     outfile.close()
         
        
--- src/create_network.py
+++ src/create_network.py
@@ -1,6 +1,7 @@
 __author__ = 'Jonathan Rubin'
 
 import networkx as nx
+import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import rcParams
@@ -16,11 +17,15 @@ def run(figures):
             weights = line[1].split(',')
             network[TF] = weights[:-1]
     
+
     G = nx.Graph()
     for TF in network:
         for i in range(0, len(network[TF])-1, 2):
             if float(network[TF][i+1]) > 0.6:
                 G.add_edge(TF,network[TF][i],weight=float(network[TF][i+1]))
+        
+    
+    
     
     edgewidth = [ d['weight'] for (u,v,d) in G.edges(data=True)] 
     #elarge=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] >0.6]       
