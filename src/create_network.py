60c15fc311f8b84f31e94c27b8ce104430a4b837
jdrubin91 <jd.rubin91@gmail.com>
2016-06-23 14:57:40 -0600

    updates

--- src/create_network.py
+++ src/create_network.py
@@ -20,7 +20,7 @@ def run(figures):
 
     G = nx.Graph()
     for TF in network:
-        for i in range(0, (len(network[TF])*.1)-2, 3):
+        for i in range(0, (int(len(network[TF])*.1))-2, 3):
             if float(network[TF][i+1]) < 0.000000001:
                 G.add_edge(TF,network[TF][i],weight=1)
         
