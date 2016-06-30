07e872582d76ad903cd875650db1a8acd3f9d667
jdrubin91 <jd.rubin91@gmail.com>
2016-06-30 11:52:50 -0600

    updates

--- src/create_SVM_files.py
+++ src/create_SVM_files.py
@@ -8,11 +8,10 @@ def run(temp,files,bidirectional,dnase):
         a = pybt.BedTool(temp + file1).cut([0,1,2]).sort()
         trackname = ['Bidirectional','DNase']
         a = a.intersect(pybt.BedTool(bidirectional).cut([0,1,2]).sort(),wao=True)
-        print "Intersected bidir"
         a = a.intersect(pybt.BedTool(dnase).cut([0,1,2]).sort(),wao=True)
-        print "Intersected DNase"
         for file2 in os.listdir(temp):
             if file1 != file2:
+                print file2
                 trackname.append(file2.split('.')[0])
                 b = pybt.BedTool(temp + file2).cut([0,1,2]).sort()
                 a = a.intersect(b,wao=True)
