252d5293a465ec9468fb0ae4104a476de4185c74
jdrubin91 <jd.rubin91@gmail.com>
2016-06-30 11:33:37 -0600

    updates

--- src/create_SVM_files.py
+++ src/create_SVM_files.py
@@ -7,12 +7,12 @@ def run(temp,files,bidirectional):
     for file1 in os.listdir(temp):
         a = pybt.BedTool(temp + file1)
         trackname = ['Bidirectional']
-        a = a.intersect(pybt.BedTool(bidirectional),c=True)
+        a = a.intersect(pybt.BedTool(bidirectional),wao=True)
         for file2 in os.listdir(temp):
             if file1 != file2:
                 trackname.append(file2.split('.')[0])
                 b = pybt.BedTool(temp + file2)
-                a = a.intersect(b,c=True)
+                a = a.intersect(b,wao=True)
                 
         print file1, len(trackname)
         a.saveas(files + file1,trackline='\t'.join(trackname))
--- src/create_bidirectional_intersect.py
+++ src/create_bidirectional_intersect.py
@@ -12,29 +12,43 @@ def parent_dir(directory):
 
 if __name__ == "__main__":
     homedir = os.path.dirname(os.path.realpath(__file__))
-    bidirectional = '/scratch/Users/joru1876/ENCFF001UWQ.bed'
+    files = ['/scratch/Users/joru1876/ENCFF001UWQ.bed','/scratch/Shares/dowell/EMG_out_files/human/SRR1552480-1_divergent_classifications.bed']
     temp = '/scratch/Shares/dowell/ENCODE/HOCOMOCODatabaseFIMO/FIMO_OUT_v10/'
     savedir = '/scratch/Users/joru1876/files/'
-    a = pybt.BedTool(bidirectional).cut([0,1,2])
-    trackname = list()
-    for folder in os.listdir(temp):
-        file1 = temp + folder + '/fimo.bed'
-        trackname.append(file1.split('/')[-2].split('_')[0])
-        b = pybt.BedTool(file1).sort()
-        a = a.intersect(b,c=True)
+    for bidirectional in files:
+        a = pybt.BedTool(bidirectional).cut([0,1,2])
+        trackname = list()
+        for folder in os.listdir(temp):
+            print folder
+            file1 = temp + folder + '/fimo.bed'
+            trackname.append(file1.split('/')[-2].split('_')[0])
+            b = pybt.BedTool(file1).sort()
+            a = a.intersect(b,c=True)
+            
+        a.saveas(savedir + bidirectional.split('/')[-1],trackline='\t'.join(trackname))
         
-    a.saveas(savedir + bidirectional.split('/')[-1],trackline='\t'.join(trackname))
-    
-    
-    bidirectional = '/scratch/Shares/dowell/EMG_out_files/human/SRR1552480-1_divergent_classifications.bed'
-    temp = '/scratch/Shares/dowell/ENCODE/HOCOMOCODatabaseFIMO/FIMO_OUT_v10/'
+    files = ['/scratch/Users/joru1876/ENCFF001UWQ.bed','/scratch/Shares/dowell/EMG_out_files/human/SRR1552480-1_divergent_classifications.bed']
+    motif = '/scratch/Shares/dowell/ENCODE/HOCOMOCODatabaseFIMO/FIMO_OUT_v10/'
+    temp = parent_dir(homedir) + '/temp/'
     savedir = '/scratch/Users/joru1876/files/'
-    a = pybt.BedTool(bidirectional).cut([0,1,2])
-    trackname = list()
-    for folder in os.listdir(temp):
-        file1 = temp + folder + '/fimo.bed'
-        trackname.append(file1.split('/')[-2].split('_')[0])
-        b = pybt.BedTool(file1).sort()
-        a = a.intersect(b,c=True)
-        
-    a.saveas(savedir + bidirectional.split('/')[-1],trackline='\t'.join(trackname))
+    
+    for bed in files:
+        a = pybt.BedTool(bed).cut([0,1,2])
+        trackname = list()
+        for file1 in temp:
+            print file1
+            for folder in motif:
+                if folder.split('_')[0] == file1:
+                    print folder
+                    name = file1.split('.')[0]
+                    trackname.append(name)
+                    trackname.append(name + '_M')
+                    b = pybt.BedTool(temp + file1).cut([0,1,2]).sort()
+                    c = pybt.BedTool(motif + folder + '/fimo.bed').cut([0,1,2]).sort()
+                    a = a.intersect(b,c=True)
+                    a = a.intersect(c,c=True)
+                    
+        a.saveas(savedir + bed.split('/')[-1],trackline='\t'.join(trackname))
+                    
+                    
+            
