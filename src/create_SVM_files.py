238ed1ab906b513810fff2a4143a6e7aae3500e1
jdrubin91 <jd.rubin91@gmail.com>
2016-06-30 11:41:38 -0600

    updates

--- src/create_SVM_files.py
+++ src/create_SVM_files.py
@@ -5,14 +5,14 @@ import os
 
 def run(temp,files,bidirectional,dnase):
     for file1 in os.listdir(temp):
-        a = pybt.BedTool(temp + file1)
+        a = pybt.BedTool(temp + file1).cut([0,1,2]).sort()
         trackname = ['Bidirectional','DNase']
-        a = a.intersect(pybt.BedTool(bidirectional),wao=True)
-        a = a.intersect(pybt.BedTool(dnase),wao=True)
+        a = a.intersect(pybt.BedTool(bidirectional).cut([0,1,2]).sort(),wao=True)
+        a = a.intersect(pybt.BedTool(dnase).cut([0,1,2]).sort(),wao=True)
         for file2 in os.listdir(temp):
             if file1 != file2:
                 trackname.append(file2.split('.')[0])
-                b = pybt.BedTool(temp + file2)
+                b = pybt.BedTool(temp + file2).cut([0,1,2]).sort()
                 a = a.intersect(b,wao=True)
                 
         print file1, len(trackname)
