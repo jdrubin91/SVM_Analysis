1ff94915cc217f860c47529b3cb47b2b3ff4d111
jdrubin91 <jd.rubin91@gmail.com>
2016-07-01 12:53:31 -0600

    updates

--- src/create_SVM_files.py
+++ src/create_SVM_files.py
@@ -16,27 +16,42 @@ def append(file1,file2):
     for line in linelist:
         outfile.write(line)
 
+def add_header(file1,header):
+    linelist = list()
+    with open(file1) as F:
+        for line in F:
+            linelist.append(line)
+            
+    outfile = open(file1,'w')
+    outfile.write(header)
+    for line in linelist:
+        outfile.write(line)
+
 def run(temp,files,bidirectional,dnase):
     for file1 in os.listdir(temp):
         if 'eGFP' not in file1:
             a = pybt.BedTool(temp + file1).cut([0,1,2]).sort()
-            a.saveas(files+file1, trackline='Chr\tStart\tStop')
+            a.saveas(files+file1)
             print files+file1
             a = pybt.BedTool(files + file1).cut([0,1,2]).sort()
             b = pybt.BedTool(bidirectional).cut([0,1,2]).sort()
             a = a.intersect(b,wao=True)
-            a.saveas(files+'temp.bed',trackline='Bidirectional')
+            a.saveas(files+'temp.bed')
             append(files+file1,files+'temp.bed')
             a = pybt.BedTool(files + file1)
             b = pybt.BedTool(dnase).cut([0,1,2]).sort()
             a = a.intersect(b,wao=True)
-            a.saveas(files+'temp.bed',trackline='DNase')
+            a.saveas(files+'temp.bed')
             append(files+file1,files+'temp.bed')
+            header = ['Bidirectional','DNase']
             for file2 in os.listdir(temp):
                 if file1 != file2 and 'eGFP' not in file2:
                     a = pybt.BedTool(files+file1)
                     print file2
+                    header.append(file2)
                     b = pybt.BedTool(temp + file2).cut([0,1,2]).sort()
                     a = a.intersect(b,wao=True)
-                    a.saveas(files+'temp.bed',trackline=file2)
+                    a.saveas(files+'temp.bed')
                     append(files+file1,files+'temp.bed')
+                    
+        add_header(files+file1,'\t'.join(header))
