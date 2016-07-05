d039dfc44b1296cbd9630db9f71a7fa48a1bf69c
jdrubin91 <jd.rubin91@gmail.com>
2016-07-05 16:11:09 -0600

    updates

--- src/create_matrix_from_bidirectional.py
+++ src/create_matrix_from_bidirectional.py
@@ -21,6 +21,8 @@ def run():
     
     files = [file1,file2]
     
+    vectors = list()
+    
     for file1 in files:
         print file1
         with open(file1) as F:
@@ -38,51 +40,52 @@ def run():
                                 vectors[i][j] += 1.0
                                 
             
-        vectors = [[i/N for i in TF] for TF in vectors]
-        vectors = np.array(vectors)
+        vector = [[i/N for i in TF] for TF in vectors]
+        vector = np.array(vectors)
         labels = [i for i in TFs if 'eGFP' not in i]
         
-        # Generate random features and distance matrix.
-        x = len(labels)
-        D = scipy.zeros([x,x])
-        for i in range(x):
-            for j in range(x):
-                for k in range(x):                    
-                    D[i,j] += (vectors[i][k] - vectors[j][k])**2
-                D[i,j] = math.sqrt(D[i,j])
-        print D
+        vectors.append(vector)
         
-        
-        # Compute and plot first dendrogram.
-        fig = pylab.figure(figsize=(8,8))
-        ax1 = fig.add_axes([0.09,0.1,0.2,0.6])
-        Y = sch.linkage(D, method='centroid')
-        Z1 = sch.dendrogram(Y, orientation='right')
-        ax1.set_xticks([])
-        ax1.set_yticks([])
-        
-        # Compute and plot second dendrogram.
-        ax2 = fig.add_axes([0.3,0.71,0.6,0.2])
-        Y = sch.linkage(D, method='single')
-        Z2 = sch.dendrogram(Y)
-        ax2.set_xticks([])
-        ax2.set_yticks([])
-        
-        # Plot distance matrix.
-        axmatrix = fig.add_axes([0.3,0.1,0.6,0.6])
-        idx1 = Z1['leaves']
-        idx2 = Z2['leaves']
-        D = D[idx1,:]
-        D = D[:,idx2]
-        im = axmatrix.matshow(D, aspect='auto', origin='lower', cmap=pylab.cm.YlGnBu)
-        axmatrix.set_xticks([])
-        axmatrix.set_yticks([])
-        
-        # Plot colorbar.
-        axcolor = fig.add_axes([0.91,0.1,0.02,0.6])
-        pylab.colorbar(im, cax=axcolor)
-        fig.show()
-        fig.savefig(savedir + file1.split('/')[-1] + '_dendrogram.png')
+    # Generate random features and distance matrix.
+    x = len(labels)
+    D = scipy.zeros([x,x])
+    for i in range(x):
+        for j in range(x):
+            for k in range(x):                    
+                D[i,j] += (vectors[1][i][k]/vectors[0][j][k])
+    print D
+    
+    
+    # Compute and plot first dendrogram.
+    fig = pylab.figure(figsize=(8,8))
+    ax1 = fig.add_axes([0.09,0.1,0.2,0.6])
+    Y = sch.linkage(D, method='centroid')
+    Z1 = sch.dendrogram(Y, orientation='right')
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    
+    # Compute and plot second dendrogram.
+    ax2 = fig.add_axes([0.3,0.71,0.6,0.2])
+    Y = sch.linkage(D, method='single')
+    Z2 = sch.dendrogram(Y)
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+    
+    # Plot distance matrix.
+    axmatrix = fig.add_axes([0.3,0.1,0.6,0.6])
+    idx1 = Z1['leaves']
+    idx2 = Z2['leaves']
+    D = D[idx1,:]
+    D = D[:,idx2]
+    im = axmatrix.matshow(D, aspect='auto', origin='lower', cmap=pylab.cm.YlGnBu)
+    axmatrix.set_xticks([])
+    axmatrix.set_yticks([])
+    
+    # Plot colorbar.
+    axcolor = fig.add_axes([0.91,0.1,0.02,0.6])
+    pylab.colorbar(im, cax=axcolor)
+    fig.show()
+    fig.savefig(savedir + '_dendrogram.png')
 
 if __name__ == "__main__":
     run()
