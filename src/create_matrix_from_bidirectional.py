5a2d674b3f60a1c7b107c5c2f43b8ccff99f50f4
jdrubin91 <jd.rubin91@gmail.com>
2016-07-08 16:12:42 -0600

    updates

--- src/create_matrix_from_bidirectional.py
+++ src/create_matrix_from_bidirectional.py
@@ -132,19 +132,19 @@ def run():
     ax2.set_xticks([])
     ax2.set_yticks([])
     c = mcolors.ColorConverter().to_rgb
-    rvb = make_colormap([c('blue'), c('white'), 0.167, c('white'), c('red'), 1, c('red')])
+    rvb = make_colormap([c('blue'), c('white'), 0.333, c('white'), c('red'), 1, c('red')])
     # Plot distance matrix.
     axmatrix = fig.add_axes([0.3,0.1,0.6,0.6])
     idx1 = Z2['leaves']
     idx2 = Z2['leaves']
     D = D[idx1,:]
     D = D[:,idx2]
-    im = axmatrix.matshow(D, aspect='auto', origin='lower', cmap=rvb,vmax=6,vmin=0)
+    im = axmatrix.matshow(D, aspect='auto', origin='lower', cmap=rvb,vmax=3,vmin=0)
     axmatrix.set_xticks([])
     #axmatrix.set_yticks([])
     axmatrix.set_yticks(range(0,vectors.shape[0]))
     axmatrix.yaxis.tick_left()
-    axmatrix.set_yticklabels([labels[val] for val in idx1],minor=False,fontsize=8)
+    axmatrix.set_yticklabels([labels[val] for val in idx1],minor=False,fontsize=4)
     
     # Plot colorbar.
     axcolor = fig.add_axes([0.91,0.1,0.02,0.6])
