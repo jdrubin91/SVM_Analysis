2806595c9ba31cf4be4ccf9aa820bd5c9cd99acb
jdrubin91 <jd.rubin91@gmail.com>
2016-07-08 11:18:23 -0600

    updates

--- src/create_matrix_from_bidirectional.py
+++ src/create_matrix_from_bidirectional.py
@@ -92,18 +92,18 @@ def run():
     # Compute and plot first dendrogram.
     D = vectors
     fig = pylab.figure(figsize=(8,8))
-    ax1 = fig.add_axes([0.09,0.1,0.2,0.6])
+    #ax1 = fig.add_axes([0.09,0.1,0.2,0.6])
     Y = sch.linkage(D, method='centroid')
     Z1 = sch.dendrogram(Y, orientation='right')
-    ax1.set_xticks([])
-    ax1.set_yticks([])
+    #ax1.set_xticks([])
+    #ax1.set_yticks([])
     
     # Compute and plot second dendrogram.
-    ax2 = fig.add_axes([0.3,0.71,0.6,0.2])
+    #ax2 = fig.add_axes([0.3,0.71,0.6,0.2])
     Y = sch.linkage(D, method='single')
     Z2 = sch.dendrogram(Y)
-    ax2.set_xticks([])
-    ax2.set_yticks([])
+    #ax2.set_xticks([])
+    #ax2.set_yticks([])
     
     # Plot distance matrix.
     axmatrix = fig.add_axes([0.3,0.1,0.6,0.6])
@@ -111,7 +111,7 @@ def run():
     idx2 = Z2['leaves']
     D = D[idx1,:]
     D = D[:,idx2]
-    im = axmatrix.matshow(D, aspect='auto', origin='lower', cmap=pylab.cm.YlGnBu)
+    im = axmatrix.matshow(D, aspect='auto', origin='lower', cmap=pylab.cm.YlGnBu,vmax=5,vmin=0)
     axmatrix.set_xticks([])
     axmatrix.set_yticks([])
     
